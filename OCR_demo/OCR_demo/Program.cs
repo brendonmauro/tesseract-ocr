90a9d59af627b97e7eaf08260130793ef954d631
MASTER <brendonmauro@hotmail.com>
2024-04-27 23:30:15 -0300

    Feat: Project created

---
+++ OCR_demo/OCR_demo/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tesseract;
+
+namespace OCR_Demo
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testImage = @"E:\ocr.png";
+
+            try
+            {
+                using (var engine = new TesseractEngine(@"tessdata", "eng", EngineMode.Default))
+                {
+                    using (var img = Pix.LoadFromFile(testImage))
+                    {
+                        using (var page = engine.Process(img))
+                        {
+                            var texto = page.GetText();
+
+                            Console.WriteLine("Taxa de Precisão: {0}", page.GetMeanConfidence());
+                            Console.WriteLine("Texto: \r\n{0}", texto);
+                        }
+                    }
+                }
+            } catch (Exception ex) {
+                Console.WriteLine("Erro: {0}", ex.Message);
+            } finally
+            {
+                Console.ReadLine();
+            }
+    }
+    }
+}
